fde5bb2de578e99f2fac4f1ee0a0c029009f22dd
Rafael Souza <rafaelrochasouza@outlook.com.br>
2020-12-09 23:24:51 -0300

    More unit tests added

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -1,25 +1,56 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CurrentWeatherComponent } from './current-weather.component';
+import { WeatherService } from '../weather/weather.service';
+import { injectSpy } from 'angular-unit-test-helper';
+import { of } from 'rxjs';
+import { fakeWeather } from '../weather/weather.service.fake';
+import { By } from '@angular/platform-browser';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
   let fixture: ComponentFixture<CurrentWeatherComponent>;
+  let weatherServiceMock: jasmine.SpyObj<WeatherService>;
 
   beforeEach(async () => {
+    const weatherServiceSpy = jasmine.createSpyObj(
+      'WeatherService', ['getCurrentWeather']);
     await TestBed.configureTestingModule({
-      declarations: [ CurrentWeatherComponent ]
+      declarations: [ CurrentWeatherComponent ],
+      providers: [{
+        provide: WeatherService, useValue: weatherServiceSpy,
+      }]
     })
     .compileComponents();
+    weatherServiceMock = injectSpy(WeatherService);
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(CurrentWeatherComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    weatherServiceMock.getCurrentWeather.and.returnValue(of());
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should get currentWeather from weatherService once', () => {
+    weatherServiceMock.getCurrentWeather.and.returnValue(of());
+    fixture.detectChanges();
+    expect(weatherServiceMock.getCurrentWeather).toHaveBeenCalledTimes(1);
+  });
+
+  it('should eagerly load currentWeather in Bethesda from weatherService', () => {
+    weatherServiceMock.getCurrentWeather.and.returnValue(of(fakeWeather));
+    fixture.detectChanges();
+    expect(component.current).toBeDefined();
+    expect(component.current.city).toEqual('Bethesda');
+    expect(component.current.temperature).toEqual(280.32);
+    const debugEl = fixture.debugElement;
+    const titleEl: HTMLElement = debugEl.query(By.css('span'))
+      .nativeElement;
+    expect(titleEl.textContent).toContain('Bethesda');
+  })
 });
